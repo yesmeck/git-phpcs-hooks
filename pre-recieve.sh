ffd2a476fe0554cc734daff0ff1f7409ff51c28b
Meck <yesmeck@gmail.com>
2012-05-06 00:46:19 +0800

    调整变量名

--- pre-recieve.sh
+++ pre-recieve.sh
@@ -7,22 +7,22 @@ TMP_DIR=$(mktemp -d)
 EMPTY_REF='0000000000000000000000000000000000000000'
 
 # Colors
-purple='\033[35m'
-red='\033[31m'
-red_bold='\033[1;31m'
-yellow='\033[33m'
-yellow_bold='\033[1;33m'
-green='\033[32m'
-green_bold='\033[1;32m'
-blue='\033[34m'
-blue_bold='\033[1;34m'
-color_end='\033[0m'
+PURPLE='\033[35m'
+RED='\033[31m'
+RED_BOLD='\033[1;31m'
+YELLOW='\033[33m'
+YELLOW_BOLD='\033[1;33m'
+GREEN='\033[32m'
+GREEN_BOLD='\033[1;32m'
+BLUE='\033[34m'
+BLUE_BOLD='\033[1;34m'
+COLOR_END='\033[0m'
 
 while read oldrev newrev ref
 do
     # 当push新分支的时候oldrev会不存在，删除时newrev就不存在
     if [[ $oldrev != $EMPTY_REF && $newrev != $EMPTY_REF ]]; then
-        echo -e "\n${purple}CodeSniffer check result:${color_end}"
+        echo -e "\n${PURPLE}CodeSniffer check result:${COLOR_END}"
         echo
         # 警告数
         warnings_count=0
@@ -70,12 +70,12 @@ do
                 msg="${file}: "
 
                 if [[ $warning > 0 ]]; then
-                    msg="$msg${yellow_bold}${warning}${color_end} ${yellow}warnings${color_end} "
+                    msg="$msg${YELLOW_BOLD}${warning}${COLOR_END} ${YELLOW}warnings${COLOR_END} "
 
                     let "warnings_count = warnings_count + 1"
                 fi
                 if [[ $error > 0 ]]; then
-                    msg="$msg${red_bold}${error}${color_end} ${red}errors${color_end}"
+                    msg="$msg${RED_BOLD}${error}${COLOR_END} ${RED}errors${COLOR_END}"
 
                     let "errors_count = errors_count + 1"
                 fi
@@ -88,12 +88,12 @@ do
         done
 
         if [[ $checked_file_count == 0 ]]; then
-            echo -e "    ${blue_bold}No file was checked.${color_end}"
+            echo -e "    ${BLUE_BOLD}No file was checked.${COLOR_END}"
         elif [[ $warnings_count == 0 && $errors_count == 0 ]]; then
-            echo -e "${green_bold}$(cowsay 'Congratulations!!!')${color_end}"
+            echo -e "${GREEN_BOLD}$(cowsay 'Congratulations!!!')${COLOR_END}"
         elif [[ $errors_count  == 0 ]]; then
             echo
-            echo -e "    ${blue}Good job, no errors were found!!!${color_end}"
+            echo -e "    ${BLUE}Good job, no errors were found!!!${COLOR_END}"
         fi
 
         echo
